3738116823c18c1153cf413281323a9d070e8779
Darsh Nanavati <dmnanavati85@gmail.com>
2026-08-19 06:51:28 +0530

    Add files via upload

---
+++ 001 Introduction.c
@@ -0,0 +1,14 @@
+/* My Introduction */
+/* Author Darsh Nanavati Date: 01/04/2025 */
+/* A simple introductory C program that displays personal information using printf(). It demonstrates the basic structure of a C program, the main() function,
+ header files, and formatted console output. */
+#include<stdio.h>
+
+int main()
+{
+	printf("Hello!!\n");
+	printf("My Name is Darsh Nanavati.\n");
+	printf("I'm in Second Year Diploma in Computer Science'.\n");
+	printf("I'm fond of Reading Books.\n");
+	printf("Thank You!!");
+}
